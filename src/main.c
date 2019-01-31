d7dc95470de2c5d4a9ffa0605273d6fd40bf7957
Mark Delk <jethrodaniel@gmail.com>
2019-01-31 03:08:15 -0600

    clean up main.c

--- src/main.c
+++ src/main.c
@@ -2,11 +2,8 @@
 
 #include "__program__.h"
 
-int main(int argc, char **argv)
+// int main(int argc, char **argv)
+int main()
 {
-  // The `(void)param` notation is used to ignore the unused variable warning
-  (void)argc;
-  (void)argv;
-
   printf("%s", hello());
 }
